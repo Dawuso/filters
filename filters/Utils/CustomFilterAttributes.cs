950cb9d7e3995aa56535953814c920930d37dc3b
Dawid Iwaniuk <dawuso@gmail.com>
2021-05-17 18:50:14 +0200

    Add project files.

---
+++ filters/Utils/CustomFilterAttributes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace filters.Utils
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next) 
+        {
+            var result = context.Result; 
+            if (result is PageResult)
+            { 
+                var page = ((PageResult)result); 
+                page.ViewData["ip"] = context.HttpContext.Connection.RemoteIpAddress;
+            }
+            await next();
+        }
+    }
+}
